4a4f5e24beff47699f26e2aaa90a04a661a44c23
Carla <carla.debeer.uk@gmail.com>
2016-11-10 11:18:42 +0000

    Updating algorithm

--- P5js.Naive Bayesian Text Classifier/sketch.js
+++ P5js.Naive Bayesian Text Classifier/sketch.js
@@ -11,10 +11,12 @@ var tokenCountA = 0;
 var tokenCountB = 0;
 var docCountA = 0;
 var docCountB = 0;
-var probA = 0;
-var probB = 0;
-var resA = 0;
-var resB = 0;
+var resA = 0.0;
+var resB = 0.0;
+var newWords;
+var result = [];
+
+var filePath;
 
 var inputList = [
 	{
@@ -27,43 +29,60 @@ var inputList = [
 	}
 ];
 
-var unknown = readTextFile("textFiles/CategoryX/horses.txt");
 
 function setup() {
-
   	noCanvas();
+}
+
+function chooseText(id) {
+
+	if (id === "choose1") {
+		filePath = "textFiles/CategoryX/horses.txt";
+	} else if (id === "choose2") {
+		filePath = "textFiles/CategoryX/colours.txt";
+	}
+
+	var unknown = readTextFile(filePath);
+
+	newWords = unknown.split(/\W+/);
 
 	for (var i = 0, l = inputList.length; i < l; ++i) {
 
 		var text = readTextFile("textFiles/" + inputList[i].path);
-
 		var object = {
 			text: text,
 			category: inputList[i].category
 		};
 
-		train(object);
-
+		countWords(object);
 	}
 
-	var words = unknown.split(/\W+/);
-	combineProbablities(words);
+	calculateProbabilities();
+
+	combineProbablities();
 
 	var container = document.getElementById("container");
-	var newParagraph = document.createElement('p');
+	var dynamicParagraph = document.getElementById("dynamicParagraph");
+
+	if (dynamicParagraph) {
+		dynamicParagraph.parentNode.removeChild(dynamicParagraph);
+	}
+
+	var newParagraph = document.createElement("p");
+	newParagraph.id = "dynamicParagraph";
 
 	if (resA > resB) {
-		newParagraph.textContent = "RESULT: There is a " + (resA * 100).toFixed(4) + "% probability of the Input Text being of Category A";
+		newParagraph.textContent = "RESULT: The Input Text belongs to CATEGORY A: RAINBOWS";
+		//"RESULT: There is a " + (resA * 100).toFixed(4) + "% probability of the Input Text being of Category A";
 	} else if (resA < resB) {
-		newParagraph.textContent = "RESULT: There is a " + (resB * 100).toFixed(4) + "% probability of the Input Text being of Category B";
+		newParagraph.textContent = "RESULT: The Input Text belongs to CATEGORY B: UNICORNS";
+		//"RESULT: There is a " + (resB * 100).toFixed(4) + "% probability of the Input Text being of Category A";
 	} else if (resA === resB) {
 		newParagraph.textContent = "RESULT: There is an equal probability of the Input Text being of either category";
 	}
 
-
 	newParagraph.classList.add("result");
 	container.appendChild(newParagraph);
-
 }
 
 function readTextFile(file) {
@@ -82,7 +101,7 @@ function readTextFile(file) {
 	return allText;
 }
 
-function train(object) {
+function countWords(object) {
 
 	var tokens = object.text.split(/\W+/);
 
@@ -98,54 +117,49 @@ function train(object) {
 		var token = tokens[i].toLowerCase();
 		if (dictionary[token] === undefined) {
 			dictionary[token] = {};
-			dictionary[token].countA = 0;
-			dictionary[token].countB = 0;
+			if (object.category === "A") {
+				dictionary[token].countA = 1;
+				dictionary[token].countB = 0;
+			} else if (object.category === "B") {
+				dictionary[token].countA = 0;
+				dictionary[token].countB = 1;
+			}
 			dictionary[token].word = token;
 		} else {
-			if (object.category === 'A') {
-				this.dictionary[token].countA++;
-				this.tokenCountA++;
-			} else if (object.category === 'B') {
-				this.dictionary[token].countB++;
-				this.tokenCountB++;
+			if (object.category === "A") {
+				dictionary[token].countA++;
+				tokenCountA++;
+			} else if (object.category === "B") {
+				dictionary[token].countB++;
+				tokenCountB++;
 			}
 		}
 	}
-
-	calculateProbabilities(tokens);
 }
 
-function calculateProbabilities(keys) {
+function calculateProbabilities() {
+	for (var key in dictionary) {
+		if (dictionary.hasOwnProperty(key)) {
 
-	// Ok, assuming we have an array of keys
-	for (var i = 0; i < keys.length; i++) {
-		var key = keys[i].toLowerCase();
-		var word = dictionary[key];
+			var object = {};
 
-		// Average frequency per document
-		// (this assumes we've counted total documents)
-		word.freqA = word.countA / docCountA;
-		word.freqB = word.countB / docCountB;
+			var word = dictionary[key];
 
-		if (isNaN( word.freqA )) {
-			word.freqA = 0;
-		} else if (isNaN( word.freqB )) {
-			word.freqB = 0;
-		}
+			result.word = word.word;
+			var freqA = word.countA / docCountA;
+			var freqB = word.countB / docCountB;
 
-		// Probability via Bayes rule
-		word.probA = word.freqA / (word.freqA + word.freqB);
-		word.probB = 1 - word.probA;
+			// Probability via Bayes rule
+			object.word = word.word;
+			object.probA = freqA / (freqA + freqB);
+			object.probB = 1 - object.probA;
 
-		// Copy results over to global variables
-		if (i = keys.length - 1) {
-			probA = word.probA;
-			probB = word.probB;
+			result.push(object);
 		}
 	}
 }
 
-function combineProbablities(words) {
+function combineProbablities() {
 
 	// Combined probabilities
 	// http://www.paulgraham.com/naivebayes.html
@@ -153,10 +167,19 @@ function combineProbablities(words) {
 	var productB = 1;
 
 	// Multiply probabilities together
-	for (var i = 0; i < words.length; i++) {
-		productA *= probA;
-		productB *= probB;
+	for (var i = 0; i < newWords.length; i++) {
+		var newWord = newWords[i];
+		for (var j = 0; j < result.length; ++j) {
+			if (result[j].word === newWord) {
+				//if (result[j].probA !== 0) {
+					productA *= result[j].probA;
+				//} if (result[j].probB !== 0) {
+					productB *= result[j].probB;
+				//}
+			}
+		}
 	}
+
 	// Apply formula
 	resA = productA / (productA + productB);
 	resB = productB / (productB + productA);
