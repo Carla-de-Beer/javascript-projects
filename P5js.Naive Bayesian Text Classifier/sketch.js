df87f2dbbde3fd92fc0e179f957d77901ae3a8b3
Carla <carla.debeer.uk@gmail.com>
2016-11-11 06:34:03 +0000

    Updating UI selection

--- P5js.Naive Bayesian Text Classifier/sketch.js
+++ P5js.Naive Bayesian Text Classifier/sketch.js
@@ -3,7 +3,6 @@
 // November 2016
 // Inspired by Daniel Shiffman's Coding Rainbow series:
 // http://shiffman.net/a2z/intro/
-
 // Built with P5.js
 
 var dictionary = {};
@@ -11,24 +10,33 @@ var tokenCountA = 0;
 var tokenCountB = 0;
 var docCountA = 0;
 var docCountB = 0;
-var resA = 0.0;
-var resB = 0.0;
+var resA = 0;
+var resB = 0;
 var newWords;
 var result = [];
+var headerText;
 
-var filePath;
-
-var inputList = [
+var trainingSet = [
 	{
-		path: "CategoryA/rainbows.txt",
+		path: "Business/B-03.txt",
 		category: "A"
-
 	}, {
-		path: "CategoryB/unicorns.txt",
+		path: "Business/B-05.txt",
+		category: "A"
+	}, {
+		path: "Business/B-09.txt",
+		category: "A"
+	},{
+		path: "Sport/S-03.txt",
+		category: "B"
+	}, {
+		path: "Sport/S-05.txt",
+		category: "B"
+	}, {
+		path: "Sport/S-09.txt",
 		category: "B"
 	}
 ];
-
 
 function setup() {
   	noCanvas();
@@ -36,59 +44,79 @@ function setup() {
 
 function chooseText(id) {
 
+	var filePath;
+	resA = 0;
+	resB = 0;
+
 	if (id === "choose1") {
-		filePath = "textFiles/CategoryX/horses.txt";
+		filePath = "textFiles/CategoryX/X-01.txt";
 	} else if (id === "choose2") {
-		filePath = "textFiles/CategoryX/colours.txt";
+		filePath = "textFiles/CategoryX/X-02.txt";
+	}  else if (id === "choose3") {
+		filePath = "textFiles/CategoryX/X-03.txt";
+	}  else if (id === "choose4") {
+		filePath = "textFiles/CategoryX/X-04.txt";
+	}  else if (id === "choose5") {
+		filePath = "textFiles/CategoryX/X-05.txt";
+	}  else if (id === "choose6") {
+		filePath = "textFiles/CategoryX/X-06.txt";
+	}  else if (id === "choose7") {
+		filePath = "textFiles/CategoryX/X-07.txt";
+	}  else if (id === "choose8") {
+		filePath = "textFiles/CategoryX/X-08.txt";
+	}  else if (id === "choose9") {
+		filePath = "textFiles/CategoryX/X-09.txt";
+	}  else if (id === "choose10") {
+		filePath = "textFiles/CategoryX/X-10.txt";
 	}
 
-	var unknown = readTextFile(filePath);
-
-	newWords = unknown.split(/\W+/);
-
-	for (var i = 0, l = inputList.length; i < l; ++i) {
-
-		var text = readTextFile("textFiles/" + inputList[i].path);
-		var object = {
-			text: text,
-			category: inputList[i].category
-		};
-
-		countWords(object);
-	}
-
-	calculateProbabilities();
-
-	combineProbablities();
-
-	var container = document.getElementById("container");
-	var dynamicParagraph = document.getElementById("dynamicParagraph");
-
-	if (dynamicParagraph) {
-		dynamicParagraph.parentNode.removeChild(dynamicParagraph);
-	}
-
-	var newParagraph = document.createElement("p");
-	newParagraph.id = "dynamicParagraph";
-
-	if (resA > resB) {
-		newParagraph.textContent = "RESULT: The Input Text belongs to CATEGORY A: RAINBOWS";
-		//"RESULT: There is a " + (resA * 100).toFixed(4) + "% probability of the Input Text being of Category A";
-	} else if (resA < resB) {
-		newParagraph.textContent = "RESULT: The Input Text belongs to CATEGORY B: UNICORNS";
-		//"RESULT: There is a " + (resB * 100).toFixed(4) + "% probability of the Input Text being of Category A";
-	} else if (resA === resB) {
-		newParagraph.textContent = "RESULT: There is an equal probability of the Input Text being of either category";
-	}
-
-	newParagraph.classList.add("result");
-	container.appendChild(newParagraph);
+	doStuff(filePath, readFirstLine);
 }
 
-function readTextFile(file) {
+function doStuff(filePath, callback) {
+
+	if (typeof callback === "function") {
+		callback(filePath);
+	}
+
+	setTimeout(function() {
+
+		var unknown = readTextFile(filePath);
+
+		// Train the classifier:
+		newWords = unknown.split(/\W+/);
+
+		for (var i = 0, l = trainingSet.length; i < l; ++i) {
+
+			var text = readTextFile("textFiles/" + trainingSet[i].path);
+			var object = {
+				text: text,
+				category: trainingSet[i].category
+			};
+
+			countWords(object);
+		}
+
+		calculateProbabilities();
+		combineProbablities();
+		renderOutput(filePath);
+
+	}, 5);
+}
+
+function readFirstLine(filePath) {
+	var XHR = new XMLHttpRequest();
+	XHR.open("GET", filePath, true);
+	XHR.send();
+	XHR.onload = function (){
+		headerText = XHR.responseText.slice(0, XHR.responseText.indexOf("\n"));
+	};
+}
+
+function readTextFile(filePath) {
 	var allText = {};
 	var rawFile = new XMLHttpRequest();
-	rawFile.open("GET", file, false);
+	rawFile.open("GET", filePath, false);
 	rawFile.onreadystatechange = function () {
 		if(rawFile.readyState === XMLHttpRequest.DONE) {
 			if(rawFile.status === 200 || rawFile.status == XMLHttpRequest.UNSENT) {
@@ -121,8 +149,8 @@ function countWords(object) {
 				dictionary[token].countA = 1;
 				dictionary[token].countB = 0;
 			} else if (object.category === "B") {
-				dictionary[token].countA = 0;
-				dictionary[token].countB = 1;
+				dictionary[token].countA = 1;
+				dictionary[token].countB = 0;
 			}
 			dictionary[token].word = token;
 		} else {
@@ -140,9 +168,7 @@ function countWords(object) {
 function calculateProbabilities() {
 	for (var key in dictionary) {
 		if (dictionary.hasOwnProperty(key)) {
-
 			var object = {};
-
 			var word = dictionary[key];
 
 			result.word = word.word;
@@ -160,7 +186,6 @@ function calculateProbabilities() {
 }
 
 function combineProbablities() {
-
 	// Combined probabilities
 	// http://www.paulgraham.com/naivebayes.html
 	var productA = 1;
@@ -171,11 +196,14 @@ function combineProbablities() {
 		var newWord = newWords[i];
 		for (var j = 0; j < result.length; ++j) {
 			if (result[j].word === newWord) {
-				//if (result[j].probA !== 0) {
-					productA *= result[j].probA;
-				//} if (result[j].probB !== 0) {
-					productB *= result[j].probB;
-				//}
+
+				if (result[j].probA !== 0) {
+					productA *= result[j].probA * 1.9;
+				}
+
+				if (result[j].probB !== 0) {
+					productB *= result[j].probB * 1.9;
+				}
 			}
 		}
 	}
@@ -183,6 +211,28 @@ function combineProbablities() {
 	// Apply formula
 	resA = productA / (productA + productB);
 	resB = productB / (productB + productA);
+}
 
+function renderOutput(filePath) {
+
+	var container = document.getElementById("container");
+	// var dynamicParagraph = document.getElementById("dynamicParagraph");
+	// if (dynamicParagraph) {
+	// 	dynamicParagraph.parentNode.removeChild(dynamicParagraph);
+	// }
+
+	var newParagraph = document.createElement("p");
+	newParagraph.id = "dynamicParagraph";
+
+	if (resA > resB) {
+		newParagraph.textContent = filePath.substring(filePath.length - 8, filePath.length) + ". '" + headerText + "': " + "Classification Result: BUSINESS";
+	} else if (resA < resB) {
+		newParagraph.textContent = filePath.substring(filePath.length - 8, filePath.length) + ". '" + headerText + "': " + "Classification Result: SPORT";
+	} else if (resA === resB) {
+		newParagraph.textContent = "RESULT: There is an equal probability of the Input Text being of either category";
+	}
+
+	newParagraph.classList.add("result");
+	container.appendChild(newParagraph);
 }
 
